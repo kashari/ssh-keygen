756281e05826ea190e08b4808ab3342c70cbbba8
Misen Kashari <93337874+kashari@users.noreply.github.com>
2023-09-10 13:33:24 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ssh-keygen
 Github Acount or else needed SSH key generation and add to SSH-Agent.
+
+## automation for local use
+Make sure you have CURL installed.
+
+```shell
+curl -fsSL https://raw.githubusercontent.com/kashari/ssh-keygen/init.sh | tee flow.sh && \
+ chmod 755 flow.sh && ./flow.sh -e "misenkashari@icloud.com"
+```
+Make sure to change the email according to your needs.
---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+EMAIL=''
+
+while getotps e: "${OPTION}"
+do
+    case "${OPTION}" in
+        e)
+            EMAIL="${OPTARG}"
+            ;;
+        ?)
+            ;;
+    esac
+done
+
+if [[ "${OPTION}" = '']]
+then
+    echo 'Please input an email as identifier.'
+    exit 1
+fi
+
+# Generate this SSH key.
+ssh-keygen -t ed25519 -C "${EMAIL}"
+
+# Start the SSH agent.
+eval "$(ssh-agent -s)"
+
+# Add the newly generated key to the SSH Agent.
+ssh-add ~/.ssh/id_ed25519
+
+cat ~/.ssh/id_ed25519.pub
+
+exit 0
